584fb34c65b8758bdbbb400714e1b5fc233ca6c7
Sanuth23 <sanuth0000@gmail.com>
2024-02-22 14:24:59 +0530

    create get mapping for book entity

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.dto.Book;
+import org.example.entity.BookEntity;
 import org.example.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,5 +18,10 @@ public class BookController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addBook(@RequestBody Book book){
         service.addBook(book);
+    }
+
+    @GetMapping
+    public Iterable<BookEntity> getBooks(){
+        return service.getBooks();
     }
 }
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -9,6 +9,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
@@ -24,5 +26,11 @@ public class BookServiceImpl implements BookService {
     public void addBook(Book book) {
         BookEntity entity = mapper.map(book, BookEntity.class);
         repository.save(entity);
+    }
+
+    @Override
+    public List<BookEntity> getBooks() {
+        return repository.findAll();
+
     }
 }
