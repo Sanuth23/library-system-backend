c8925027a3b60725edcf63ccdaa0e015d8e886e9
Sanuth23 <sanuth0000@gmail.com>
2024-02-22 13:51:15 +0530

    change http status

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -3,10 +3,8 @@ package org.example.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.Book;
 import org.example.service.BookService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -16,6 +14,7 @@ public class BookController {
     final BookService service;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void addBook(@RequestBody Book book){
         service.addBook(book);
     }
